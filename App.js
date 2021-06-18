aea79914149fda23caf732afbd09fb492e855175
Jhonatan mendes <jhowfrombrazil@gmail.com>
2021-06-18 13:09:06 -0300

    set up the react navigator

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import MealsNavigator from "./navigation/MealsNavigator";
 
 export default function App() {
-  return (
-    <View>
-      <Text>hi HI HI HI HI</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [loaded, error] = Font.useFonts({
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
+  return <MealsNavigator />;
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  font: {
+    fontFamily: "open-sans-bold",
+  },
+});
